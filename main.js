622e962a7aaf7b9eaa8f82e5d1118a2b30e22894
Bossofgym <94777676+Bossofgym@users.noreply.github.com>
2022-02-28 21:22:24 +0500

    create site

---
+++ main.js
@@ -0,0 +1,15 @@
+let body = document.getElementById("bd");
+
+
+function night(){
+    body.classList.remove("day")
+    body.classList.add("night")
+
+    
+}
+function day(){
+    body.classList.remove("night")
+    body.classList.add("day")
+
+    
+}
